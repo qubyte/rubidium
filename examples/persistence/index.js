1fb1aac2cd6e4368bc2beac05046a324fb84a387
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-09-16 13:05:51 +0100

    Updates modules for two space indentation.

--- examples/persistence/index.js
+++ examples/persistence/index.js
@@ -12,26 +12,26 @@ const rb = new Rubidium();
 
 // Load the persisted queue when redis is ready.
 redis.hgetall('rubidium-queue').then(stringifiedJobs => {
-    for (const uuid of Object.keys(stringifiedJobs)) {
-        // Add the persisted jobs to the queue, taking care to silence them so
-        // they are not duplicated in redis.
-        rb.add(JSON.parse(stringifiedJobs[uuid]), true);
-    }
+  for (const uuid of Object.keys(stringifiedJobs)) {
+    // Add the persisted jobs to the queue, taking care to silence them so
+    // they are not duplicated in redis.
+    rb.add(JSON.parse(stringifiedJobs[uuid]), true);
+  }
 });
 
 rb.on('addJob', job => {
-    redis.hset('rubidium-queue', job.uuid, JSON.stringify(job)).then(() => {
-        console.log(`Job ${job.uuid} persisted.`);
-    });
+  redis.hset('rubidium-queue', job.uuid, JSON.stringify(job)).then(() => {
+    console.log(`Job ${job.uuid} persisted.`);
+  });
 });
 
 rb.on('job', job => {
-    redis.hdel('rubidium-queue', job.uuid);
-    console.log(`Job ${job.uuid} emitted, message: ${job.message}`);
+  redis.hdel('rubidium-queue', job.uuid);
+  console.log(`Job ${job.uuid} emitted, message: ${job.message}`);
 });
 
 rb.on('removeJob', job => {
-    redis.hdel('rubidium-queue', job.uuid);
+  redis.hdel('rubidium-queue', job.uuid);
 });
 
 rb.add({ time: Date.now() + 10000, message: '10 seconds have passed.' });
